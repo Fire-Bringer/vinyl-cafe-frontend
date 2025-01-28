d114c5d8e1b0deee148f87d86bb12cb72edb3eea
Rashad DuPaty <radupaty@gmail.com>
2025-01-28 18:33:22 +0900

    add props for Hero section images

---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+// Hero.tsx
+export interface HeroProps {
+  loadImage: {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+  }[];
+  heroImage: {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+  }[];
+}
