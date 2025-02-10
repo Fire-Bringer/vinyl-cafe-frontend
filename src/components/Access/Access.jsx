a1bdee98d6b471e2c643cd1519ea8f83abf9a0db
Rashad DuPaty <radupaty@gmail.com>
2025-02-10 21:41:12 +0900

    Add map link

--- src/components/Access/Access.jsx
+++ src/components/Access/Access.jsx
@@ -4,7 +4,9 @@ const Access = () => {
 
       <h2 className="font-display text-4xl">Access</h2>
 
-      <img src="/map.png" alt="Map image" className="mt-4 w-4/5 xl:w-3/5 rounded-[20px] shadower"/>
+      <a href="https://www.google.com/maps/place/Kanazawa+Station/@36.5780443,136.6481714,17z/data=!3m1!4b1!4m6!3m5!1s0x5ff8334203bb8605:0x5d5df6011ebba7ea!8m2!3d36.5780443!4d136.6481714!16zL20vMGZ3bnZr?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D" target="_blank" className="mt-4 w-4/5 xl:w-3/5 shadower">
+        <img src="/map.png" alt="Map image" className="rounded-[20px]"/>
+      </a>
 
       <h5 className="mt-6">7-77-77 Dopeland Kanazawa</h5>
 
